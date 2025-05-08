64ec4f789e74698e69f7907eafc9dff66c79c968
just4vegan <adrian.schultze@proveg.org>
2025-05-08 15:02:20 +0200

    add draucla

--- init.lua
+++ init.lua
@@ -755,6 +755,7 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
+        ruff = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
