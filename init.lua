bc085308a03b3ac92a908c180623b9ba92218667
antifragileathlete <adrianschultze@gmail.com>
2026-02-03 08:35:03 +0100

    added stuff

--- init.lua
+++ init.lua
@@ -984,6 +984,36 @@ require('lazy').setup({
     end,
   },
   {
+    'epwalsh/obsidian.nvim',
+    version = '*', -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = 'markdown',
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    --   -- refer to `:h file-pattern` for more examples
+    --   "BufReadPre path/to/my-vault/*.md",
+    --   "BufNewFile path/to/my-vault/*.md",
+    -- },
+    dependencies = {
+      -- Required.
+      'nvim-lua/plenary.nvim',
+
+      -- see below for full list of optional dependencies 👇
+    },
+    opts = {
+      workspaces = {
+        {
+          name = 'it',
+          path = '~/vaults/it',
+        },
+      },
+
+      -- see below for full list of options 👇
+    },
+  },
+  {
     'm4xshen/hardtime.nvim',
     lazy = false,
     dependencies = { 'MunifTanjim/nui.nvim' },
@@ -1095,6 +1125,7 @@ vim.keymap.set('n', '<S-PageDown>', ':bprevious<CR>', { noremap = true, silent =
 vim.keymap.set('n', '<leader>gi', ':e /home/adrian/neorg/main/idx.norg<CR>', { desc = 'Go to Neorg Index' })
 vim.keymap.set('n', '<leader>oa', ':e /home/adrian/neorg/it/oactions.norg<CR>', { desc = 'Go to Odoo Next Actions' })
 vim.keymap.set('n', '<leader>ba', ':e /home/adrian/neorg/br/bactions.norg<CR>', { desc = 'Go to BR Next Actions' })
+vim.keymap.set('n', '<leader>ma', ':e /home/adrian/neorg/misc/mactions.norg<CR>', { desc = 'Go to Misc Next Actions' })
 vim.keymap.set('i', '<C-j>', '<Plug>(neorg.itero.next-iteration)', {
   noremap = true,
   silent = true,
